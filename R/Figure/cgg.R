07cc9a9865b53de84e68a80cb4bf3ba3c240fec3
Kurnia Susvitasari <ksusvita@sfu.ca>
2023-08-15 13:16:20 -0700

    The pdf of CGG with various sampling proportion.

---
+++ R/Figure/cgg.R
@@ -0,0 +1,44 @@
+# Create illustrative figure for the CGG distribution with various values
+# of pi's.
+
+
+library(siestim)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+
+
+# +++++++++++++++++++++++++++
+par <- c(6, 1) # mu, sigma;
+pi <- seq(.1, 1, .1) # sequence of pi
+
+
+x <- seq(0, 25, length.out = 100)
+fx <- sapply(pi, function(p) dcgg(x, par[1], par[2], p)) # pdf of cgg for each pi
+px <- sapply(pi, function(p) pcgg(x, par[1], par[2], p)) # cdf of cgg for each pi
+
+dt <- data.frame(x = rep(x, length(pi)),
+                 fx = as.vector(fx), px = as.vector(px),
+                 pi = rep(pi, each = length(x)))
+
+
+# ++++++++++++++++++++++++++=
+plt <- ggplot() +
+  geom_line(aes(x = x, y = fx, group = pi, col = as.factor(pi)), dt) +
+  geom_line(aes(x = x, y = dgamma(x, (par[1]/par[2])^2, par[1]/par[2]^2)), lty = "dashed") +
+  labs(y = "density", x = "") + theme_light() +
+  theme(legend.position = "bottom")
+
+plt2 <- ggplot() +
+  geom_line(aes(x = x, y = px, group = pi, col = as.factor(pi)), dt) +
+  geom_line(aes(x = x, y = pgamma(x, (par[1]/par[2])^2, par[1]/par[2]^2)), lty = "dashed") +
+  labs(y = "cumulative density", x = "") + theme_light() +
+  theme(legend.position = "bottom")
+
+(plt+plt2) + plot_layout(nrow = 1, guides = "collect") &
+  theme(legend.title = element_blank(),
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.position = "bottom")
+
