aeda8c061f31020b6bb36a55b27bb52211829ab7
Kurnia Susvitasari <ksusvita@sfu.ca>
2023-08-15 13:16:39 -0700

    The dynamics of the SIR simulation.

---
+++ R/Figure/SIR dynamics.R
@@ -0,0 +1,36 @@
+# Create the dynamics of the SIR simulation
+
+source("./R/SIR Simulation/simOutbreak.R")
+
+
+library(ggplot2)
+
+
+# ***************************************
+# parameters
+gamma <- c(4.5,2) # Generation time
+p <- .1 # sampling proportion
+epsilon <- 88 # genomic distance cutoff
+
+
+
+# ****************************************
+# Generate SIR outbreak
+set.seed(1)
+outbreak <- simOutbreak(1000, gamma, R0=2)
+
+# plot the dynamic
+ggplot(outbreak$dynam %>% mutate(t = 0:100)) +
+  geom_line(aes(t, nsus, col = "0")) +
+  geom_line(aes(t, ninf, col = "1")) +
+  geom_line(aes(t, nrec, col = "2")) +
+  scale_color_manual(values = c("0" = "darkgreen",
+                                "1" = "red",
+                                "2" = "darkblue"),
+                     labels = c("0" = "Susceptible",
+                                "1" = "Infected",
+                                "2" = "Recovered"),
+                     name = "") +
+  labs(x = "days", y = "count") +
+  theme_light() + theme(legend.position = "bottom",
+                        legend.text = element_text(size = 11))
